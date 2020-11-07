d404f49eab35176b59ba5fc2b30bf5b5f6862a46
Joe Lynch <joe.r.d.lynch@gmail.com>
2020-11-07 15:17:56 +0000

    Update TwoSum.java

--- 01_TwoSum/TwoSum.java
+++ 01_TwoSum/TwoSum.java
@@ -8,7 +8,7 @@ public class TwoSum {
             for(int i=0; i<nums.length; i++){
                 int diff = target - nums[i];
                 if(map.containsKey(diff)){
-                    return new int[]{i, map.get(diff)};
+                    return new int[]{map.get(diff), i};
                 }
                 map.put(nums[i], i);
             }
@@ -19,7 +19,7 @@ public class TwoSum {
     public static void main(String[] args){
         TwoSum solution = new TwoSum();
         assert(Arrays.equals(solution.twoSum(new int[]{2,7,11,15}, 9), new int[]{0,1}));
-        assert(Arrays.equals(solution.twoSum(new int[]{3,2,4}, 6), new int[]{1,2})));
-        assert(Arrays.equals(solution.twoSum(new int[]{3,3}, 6), new int[]{0,1})));
+        assert(Arrays.equals(solution.twoSum(new int[]{3,2,4}, 6), new int[]{1,2}));
+        assert(Arrays.equals(solution.twoSum(new int[]{3,3}, 6), new int[]{0,1}));
     }
 }
