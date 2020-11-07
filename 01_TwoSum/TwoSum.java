66bbc9f4d13817820b93bca12a742e88039e8890
Joe Lynch <joe.r.d.lynch@gmail.com>
2020-11-07 15:15:35 +0000

    Update TwoSum.java

--- 01_TwoSum/TwoSum.java
+++ 01_TwoSum/TwoSum.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class TwoSum {
 
@@ -19,7 +18,8 @@ public class TwoSum {
 
     public static void main(String[] args){
         TwoSum solution = new TwoSum();
-        int[] result = solution.twoSum(new int[]{2,7,11,15}, 9);
-        assert (Arrays.equals(result, new int[]{0,1}));
+        assert(Arrays.equals(solution.twoSum(new int[]{2,7,11,15}, 9), new int[]{0,1}));
+        assert(Arrays.equals(solution.twoSum(new int[]{3,2,4}, 6), new int[]{1,2})));
+        assert(Arrays.equals(solution.twoSum(new int[]{3,3}, 6), new int[]{0,1})));
     }
 }
