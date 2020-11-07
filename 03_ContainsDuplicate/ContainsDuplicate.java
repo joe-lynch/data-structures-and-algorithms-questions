0c02481a35556f52681816d55b048bfc567d97de
Joe Lynch <joe.r.d.lynch@gmail.com>
2020-11-07 15:21:48 +0000

    Update ContainsDuplicate.java

--- 03_ContainsDuplicate/ContainsDuplicate.java
+++ 03_ContainsDuplicate/ContainsDuplicate.java
@@ -17,7 +17,8 @@ public class ContainsDuplicate {
 
     public static void main(String[] args) {
         ContainsDuplicate solution = new ContainsDuplicate();
-        boolean result = solution.containsDuplicate(new int[]{1,2,3,1});
-        assert(result == true);
+        assert(solution.containsDuplicate(new int[]{1,2,3,1}) == true);
+        assert(solution.containsDuplicate(new int[]{1,2,3,4}) == false);
+        assert(solution.containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}) == true);
     }
 }
