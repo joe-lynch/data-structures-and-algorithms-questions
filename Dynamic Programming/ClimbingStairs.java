eed0dc734af1e59bab513413413efeae12254a06
joe-lynch <jl2553@bath.ac.uk>
2020-11-08 02:02:31 +0000

    dp problems

---
+++ Dynamic Programming/ClimbingStairs.java
@@ -0,0 +1,21 @@
+package DynamicProgramming;
+
+public class ClimbingStairs {
+
+    private int climbStairs(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs solution = new ClimbingStairs();
+        assert(solution.climbStairs(2) == 2);
+        assert(solution.climbStairs(3) == 3);
+        assert(solution.climbStairs(10) == 89);
+    }
+}
---
+++ Dynamic Programming/CoinChange.java
@@ -0,0 +1,31 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class CoinChange {
+
+    private int coinChange(int[] coins, int amount){
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+
+        for(int i=1; i<amount+1; i++){
+            for(int coin : coins) {
+                if(i >= coin){
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == amount+1 ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        CoinChange solution = new CoinChange();
+        assert(solution.coinChange(new int[]{1,2,5}, 11) == 3);
+        assert(solution.coinChange(new int[]{2}, 3) == -1);
+        assert(solution.coinChange(new int[]{1}, 0) == 0);
+        assert(solution.coinChange(new int[]{1}, 1) == 1);
+        assert(solution.coinChange(new int[]{1}, 2) == 2);
+        assert(solution.coinChange(new int[]{1,2,5,7}, 23) == 4);
+    }
+}
