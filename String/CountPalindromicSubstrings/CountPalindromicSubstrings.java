329562d35820ade586e05ea6c4d90569d6f11574
joe-lynch <jl2553@bath.ac.uk>
2020-11-14 20:53:52 +0000

    added count palindromic substrings

---
+++ String/CountPalindromicSubstrings/CountPalindromicSubstrings.java
@@ -0,0 +1,29 @@
+public class CountPalindromicSubstrings {
+
+    private int countSubstring(String s){
+        if(s.length() == 1)
+            return 1;
+        int result = 0;
+        for(int i=0; i<s.length(); i++){
+            result += palindrome(i, i, s);
+            result += palindrome(i, i+1, s);
+        }
+        return result;
+    }
+
+    private int palindrome(int left, int right, String s){
+        int tmp = 0;
+        while(left >= 0 && right< s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+            tmp++;
+        }
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+        CountPalindromicSubstrings solution = new CountPalindromicSubstrings();
+        assert(solution.countSubstring("abc") == 3);
+        assert(solution.countSubstring("aaa") == 6);
+    }
+}
