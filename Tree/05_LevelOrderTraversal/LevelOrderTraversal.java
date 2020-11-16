c3bbd5df77f373c6b3b3750001884f5bf0cbfda3
joe-lynch <jl2553@bath.ac.uk>
2020-11-16 18:39:51 +0000

    added DFS for LevelOrderTraversal

--- Tree/05_LevelOrderTraversal/LevelOrderTraversal.java
+++ Tree/05_LevelOrderTraversal/LevelOrderTraversal.java
@@ -4,13 +4,12 @@ public class LevelOrderTraversal {
     private static final int DFS = 0;
     private static final int BFS = 1;
 
-
     public List<List<Integer>> levelOrder(TreeNode root, int TYPE){
         switch(TYPE){
             case BFS:
-                levelOrderBFS(root);
+                return levelOrderBFS(root);
             case DFS:
-                levelOrderDFS(root);
+                return levelOrderDFS(root, 0);
             default:
                 throw new IllegalArgumentException("TYPE must be value 0 or 1.");
         }
@@ -50,17 +49,29 @@ public class LevelOrderTraversal {
         return list;
     }
 
-    public List<List<Integer>> levelOrderDFS(TreeNode root){
-        return null;
+    List<List<Integer>> list = new ArrayList<>();
+    public List<List<Integer>> levelOrderDFS(TreeNode root, int level){
+        if(root == null)
+            return list;
+
+        if(list.size() == level)
+            list.add(new ArrayList<>());
+
+        list.get(level).add(root.val);
+
+        levelOrderDFS(root.left, level+1);
+        levelOrderDFS(root.right, level+1);
+        return list;
     }
 
     public static void main(String[] args) {
         LevelOrderTraversal solution = new LevelOrderTraversal();
 
         List<List<Integer>> ans = Arrays.asList(Arrays.asList(3), Arrays.asList(9, 20), Arrays.asList(15, 7));
-        //List<List<Integer>> soln = solution.levelOrder(TreeNode.deserialize("3,9,20,null,null,15,7"), DFS);
-        List<List<Integer>> soln = solution.levelOrder(TreeNode.deserialize("3,9,20,null,null,15,7"), BFS);
+        List<List<Integer>> solnDFS = solution.levelOrder(TreeNode.deserialize("3,9,20,null,null,15,7"), DFS);
+        List<List<Integer>> solnBFS = solution.levelOrder(TreeNode.deserialize("3,9,20,null,null,15,7"), BFS);
 
-        assert(soln.equals(ans));
+        assert(solnDFS.equals(ans));
+        assert(solnBFS.equals(ans));
     }
 }
