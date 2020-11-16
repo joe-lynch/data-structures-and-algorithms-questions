abc5a9acd6484fdb6bf339b6f5d2819ded850019
joe-lynch <jl2553@bath.ac.uk>
2020-11-16 20:22:22 +0000

    added SumOfTwoIntegers to Binary

---
+++ Binary/01_SumOfTwoIntegers/SumOfTwoIntegers.java
@@ -0,0 +1,17 @@
+public class SumOfTwoIntegers {
+
+    private int getSum(int a, int b) {
+        while(b != 0){
+            int ans = a ^ b;
+            int carry = (a & b) << 1;
+            a = ans;
+            b = carry;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        SumOfTwoIntegers solution = new SumOfTwoIntegers();
+        assert(solution.getSum(1, 2) == 3);
+    }
+}
