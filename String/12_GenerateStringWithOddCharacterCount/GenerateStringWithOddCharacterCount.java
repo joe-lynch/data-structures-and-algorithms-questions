a1ce5af84d411ab302fd1a4c3b112ca85ca49e6f
joe-lynch <jl2553@bath.ac.uk>
2020-11-21 14:22:33 +0000

    added generate string with odd character counts

---
+++ String/12_GenerateStringWithOddCharacterCount/GenerateStringWithOddCharacterCount.java
@@ -0,0 +1,22 @@
+public class GenerateStringWithOddCharacterCount {
+
+    public String generateTheString(int N){
+        StringBuilder sb = new StringBuilder();
+
+        if(N%2 == 0){
+            sb.append("a".repeat(Math.max(0, N - 1)));
+            sb.append("b");
+        }
+        else{
+            sb.append("a".repeat(Math.max(0, N)));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        GenerateStringWithOddCharacterCount solution = new GenerateStringWithOddCharacterCount();
+        assert(solution.generateTheString(4).equals("aaab"));
+        assert(solution.generateTheString(2).equals("ab"));
+        assert(solution.generateTheString(7).equals("aaaaaaa"));
+    }
+}
